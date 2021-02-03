6e1ecf0be05e6daad758590d47f656a8cce1e6c8
Sven-Erik Staaf <sestaaf@live.se>
2021-02-03 10:48:10 +0100

    La till kommentarer i extraklass.

--- SkalProj_Datastrukturer_Minne/ListMethod.cs
+++ SkalProj_Datastrukturer_Minne/ListMethod.cs
@@ -6,5 +6,7 @@ namespace SkalProj_Datastrukturer_Minne
 {
 	class ListMethod
 	{
+		// Skulle hels vilja få över metoder Add, Remove och Show hit,
+		// men det får vänta. Vill få uppgifterna att fungera initiellt.
     }
 }
