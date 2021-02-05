fccee88df894f898c436242abd3007b4419b6375
Sven-Erik Staaf <sestaaf@live.se>
2021-02-05 11:00:38 +0100

    Nästan klar med uppgift 3.

--- SkalProj_Datastrukturer_Minne/Methods.cs
+++ SkalProj_Datastrukturer_Minne/Methods.cs
@@ -6,70 +6,6 @@ namespace SkalProj_Datastrukturer_Minne
 {
 	class Methods
 	{
-		public static void TestQueue(Queue<string> queueICA)
-		{
-            bool backToMainMenu = false;
-            char nav = ' ';
-
-            do
-            {
-                Console.WriteLine("\nMenyalternativ 1 = Ställer Kalle och Greta i kö."
-                    + "\nMenyalternativ 2 = Ställer Stina och Olle i kö."
-                    + "\nMenyalternativ 3 = En person lämnar kön"
-                    + "\n\nEnter Q to be returned to the Main Menu.");
-                Console.Write("Input > ");
-
-                string input = Console.ReadLine();
-
-                try
-                {
-                    nav = input[0];
-                }
-                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
-                {
-                    Console.Clear();
-                    Console.WriteLine("\nPlease enter some input!");
-                }
-
-                switch (nav)
-                {
-                    case '1': // Ställer Kalle och Greta i kö.
-                        queueICA.Enqueue("Kalle");
-                        queueICA.Enqueue("Greta");
-                        break;
-                    case '2': // Ställer Stina och Olle i kö.
-                        queueICA.Enqueue("Stina");
-                        queueICA.Enqueue("Olle");
-                        break;
-                    case '3': // Tar bort en person ur kön.
-                        if (queueICA.Count != 0) queueICA.Dequeue();
-                        break;
-                    case 'Q': // Avslutar meny.
-                    case 'q':
-                        backToMainMenu = true;
-                        break;
-                    default:
-                        break;
-                }
-
-                ShowQueue(queueICA);
-
-            } while (!backToMainMenu);
-        }
-
-        private static void ShowQueue(Queue<string> showQueueICA)
-        {
-            Console.Write($"\nPeople in Queue: {showQueueICA.Count}");
-            Console.Write($"\nWho in Queue: ");
-            PrintValues(showQueueICA);
-        }
-        private static void PrintValues(IEnumerable<string> printQueueICA)
-        {
-            foreach (string customer in printQueueICA)
-                Console.Write($"{customer} ");
-            Console.WriteLine();
-        }
-
 		public static void ListTest(List<string> theList)
 		{
             AddToList("First");
@@ -158,6 +94,132 @@ namespace SkalProj_Datastrukturer_Minne
             }
         }
 
-    }
+		public static void TestQueue(Queue<string> queueICA)
+		{
+            bool backToMainMenu = false;
+            char nav = ' ';
+
+            do
+            {
+                Console.WriteLine("\nMenyalternativ 1 = Ställer Kalle och Greta i kö."
+                    + "\nMenyalternativ 2 = Ställer Stina och Olle i kö."
+                    + "\nMenyalternativ 3 = En person lämnar kön"
+                    + "\n\nEnter Q to be returned to the Main Menu.");
+                Console.Write("Input > ");
+
+                string input = Console.ReadLine();
+
+                try
+                {
+                    nav = input[0];
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+                {
+                    Console.Clear();
+                    Console.WriteLine("\nPlease enter some input!");
+                }
+
+                switch (nav)
+                {
+                    case '1': // Ställer Kalle och Greta i kö.
+                        queueICA.Enqueue("Kalle");
+                        queueICA.Enqueue("Greta");
+                        break;
+                    case '2': // Ställer Stina och Olle i kö.
+                        queueICA.Enqueue("Stina");
+                        queueICA.Enqueue("Olle");
+                        break;
+                    case '3': // Tar bort en person ur kön.
+                        if (queueICA.Count != 0) queueICA.Dequeue();
+                        break;
+                    case 'Q': // Avslutar meny.
+                    case 'q':
+                        backToMainMenu = true;
+                        break;
+                    default:
+                        break;
+                }
+
+                ShowQueue(queueICA);
+
+            } while (!backToMainMenu);
+            static void ShowQueue(Queue<string> showQueueICA)
+            {
+                Console.Write($"\nPeople in Queue: {showQueueICA.Count}");
+                Console.Write($"\nWho in Queue: ");
+                PrintValues(showQueueICA);
+            }
+            static void PrintValues(IEnumerable<string> printQueueICA)
+            {
+                foreach (string customer in printQueueICA)
+                    Console.Write($"{customer} ");
+                Console.WriteLine();
+            }
+        }
+
+		internal static void TestStack(Stack<string> queueICA)
+		{
+            bool backToMainMenu = false;
+            char nav = ' ';
+
+            do
+            {
+                Console.WriteLine("\nMenyalternativ 1 = Ställer Kalle och Greta i kö."
+                    + "\nMenyalternativ 2 = Ställer Stina och Olle i kö."
+                    + "\nMenyalternativ 3 = En person lämnar kön"
+                    + "\n\nEnter Q to be returned to the Main Menu.");
+                Console.Write("Input > ");
+
+                string input = Console.ReadLine();
+
+                try
+                {
+                    nav = input[0];
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+                {
+                    Console.Clear();
+                    Console.WriteLine("\nPlease enter some input!");
+                }
+
+                switch (nav)
+                {
+                    case '1': // Ställer Kalle och Greta i kö.
+                        queueICA.Push("Kalle");
+                        queueICA.Push("Greta");
+                        break;
+                    case '2': // Ställer Stina och Olle i kö.
+                        queueICA.Push("Stina");
+                        queueICA.Push("Olle");
+                        break;
+                    case '3': // Tar bort en person ur kön.
+                        if (queueICA.Count != 0) queueICA.Pop();
+                        break;
+                    case 'Q': // Avslutar meny.
+                    case 'q':
+                        backToMainMenu = true;
+                        break;
+                    default:
+                        break;
+                }
+
+                ShowQueue(queueICA);
+
+            } while (!backToMainMenu);
+
+            static void ShowQueue(Stack<string> showQueueICA)
+            {
+                Console.Write($"\nPeople in Queue: {showQueueICA.Count}");
+                Console.Write($"\nWho in Queue: ");
+                PrintValues(showQueueICA);
+            }
+            static void PrintValues(IEnumerable<string> printQueueICA)
+            {
+                foreach (string customer in printQueueICA)
+                    Console.Write($"{customer} ");
+                Console.WriteLine();
+            }
+        }
+	}
 }
 
--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -107,7 +107,6 @@ namespace SkalProj_Datastrukturer_Minne
              */
 
             List<string> theList = new List<string>();
-
             Methods.ListTest(theList);
         }
 
@@ -123,7 +122,6 @@ namespace SkalProj_Datastrukturer_Minne
             */
 
             Queue<string> queueICA = new Queue<string>();
-
             Methods.TestQueue(queueICA);
         }
 
@@ -138,66 +136,13 @@ namespace SkalProj_Datastrukturer_Minne
              * Make sure to look at the stack after pushing and and poping to see how it behaves
             */
 
-            bool backToMainMenu = false;
-            char nav = ' ';
-            Queue<string> queueICA = new Queue<string>();
+            // Svar Fråga: Kassörskan blir i princip aldrig av med Kalle (i förra uppgiften)
+            //             och de som kommer in efter Kalle kanske aldrig får betala och gå hem.
 
-            do
-            {
-                Console.WriteLine("\nMenyalternativ 1 = Ställer Kalle och Greta i kö."
-                    + "\nMenyalternativ 2 = Ställer Stina och Olle i kö."
-                    + "\nMenyalternativ 3 = En person lämnar kön"
-                    + "\n\nEnter Q to be returned to the Main Menu.");
-                Console.Write("Input > ");
+            Stack<string> queueICA = new Stack<string>();
+            Methods.TestStack(queueICA);
 
-                string input = Console.ReadLine();
 
-                try
-                {
-                    nav = input[0];
-                }
-                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
-                {
-                    Console.Clear();
-                    Console.WriteLine("\nPlease enter some input!");
-                }
-
-                switch (nav)
-                {
-                    case '1': // Ställer Kalle och Greta i kö.
-                        queueICA.Enqueue("Kalle");
-                        queueICA.Enqueue("Greta");
-                        break;
-                    case '2': // Ställer Stina och Olle i kö.
-                        queueICA.Enqueue("Stina");
-                        queueICA.Enqueue("Olle");
-                        break;
-                    case '3': // Tar bort en person ur kön.
-                        if (queueICA.Count != 0) queueICA.Dequeue();
-                        break;
-                    case 'Q': // Avslutar meny.
-                    case 'q':
-                        backToMainMenu = true;
-                        break;
-                    default:
-                        break;
-                }
-
-                ShowQueue(queueICA);
-
-            } while (!backToMainMenu);
-        }
-        private static void ShowQueue(Queue<string> showQueueICA)
-        {
-            Console.Write($"\nPeople in Queue: {showQueueICA.Count}");
-            Console.Write($"\nWho in Queue: ");
-            PrintValues(showQueueICA);
-        }
-        private static void PrintValues(IEnumerable<string> printQueueICA)
-        {
-            foreach (string customer in printQueueICA)
-                Console.Write($"{customer} ");
-            Console.WriteLine();
         }
             static void CheckParanthesis()
         {
